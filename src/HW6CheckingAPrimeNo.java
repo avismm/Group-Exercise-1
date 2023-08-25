f6eca102556917dfc6a95e3e4d5e5c4ca4d5a6fd
avismm <amuhwerane@gmail.com>
2023-08-25 07:11:04 +0700

    Updated Exercise1, No 6

--- src/HW6CheckingAPrimeNo.java
+++ src/HW6CheckingAPrimeNo.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
+
 //Write a java program to check whether a given number is prime or
 //not?
-public class HW6PrimeNo {
+public class HW6CheckingAPrimeNo {
     public static void main(String[] args) {
-        int number = 9;
+        Scanner input =new Scanner(System.in);
+        int number;
+        do{
+            System.out.println("Please enter a number greater than 1"); // Prime numbers must be greater than 1
+            number=input.nextInt();
+        }while(number<=1);
+
         boolean prime = false;
         for (int i = 2; i <= number / 2; ++i) {
             if (number % i == 0) {
                 prime = true;
-               break;
+                break;
             }
         }
-        if (!prime)
+        if (!prime) {
             System.out.println(number + " is a prime number.");
-        else
+    }else
             System.out.println(number + " is not a prime number.");
         
     }
